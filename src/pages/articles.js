19f067e047503b69b1a586bf603e8a3babeea309
Kornel Cygan <31730479+KornelCygan@users.noreply.github.com>
2020-05-04 23:50:47 +0200

    getting data for articles from mdx

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,10 +1,33 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 
-const ArticlesPage = () => (
+const ArticlesPage = ({ data }) => (
+    <>
+        <h1>Articles Page</h1>
+        {data.allMdx.nodes.map(item => (
+            <>
+                <h2>{item.frontmatter.title}</h2>
+                <p>{item.frontmatter.slug}</p>
+                <p>{item.frontmatter.author}</p>
+            </>
+        ))}
+    </>
+);
 
-    <h1>Articles Page</h1>
-  
-)
+export const query = graphql`
+{
+    allMdx {
+        nodes {
+        frontmatter {
+            title
+            slug
+            author
+        }
+        excerpt(pruneLength: 50)
+        }
+    }
+}
+`;
 
 export default ArticlesPage
